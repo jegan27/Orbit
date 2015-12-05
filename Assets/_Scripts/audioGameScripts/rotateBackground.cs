df3e8b7c3e52bf25ca881b1becc7b07e3dce32da
Cameron Walsh <cjwalsh@live.com.au>
2015-12-05 11:35:00 +1100

    AudioGame
    Added aesthetic assets

---
+++ Assets/_Scripts/audioGameScripts/rotateBackground.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotateBackground : MonoBehaviour {
+
+	// Use this for initialization
+	public float speed = 10f;
+	
+	
+	void Update ()
+	{
+		transform.Rotate(Vector3.back, speed * Time.deltaTime);
+	}
+}
