4dc46399fe4fef49a5810f188094fb4758a1c71c
jegan27 <111443@student.sae.edu.au>
2015-12-08 12:23:32 +1100

    Added Screen Resolutions and fixed a few bugs

---
+++ Assets/_Scripts/ScreenOrientation.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenOrientation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		Screen.orientation = UnityEngine.ScreenOrientation.Portrait;
+	}
+	
+
+}
